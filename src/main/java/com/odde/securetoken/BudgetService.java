5dd5e34861a24a5938fc62489a55741076b30c44
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-08-08 13:07:40 +0800

    :recycle: remove unneeded cross year

--- src/main/java/com/odde/securetoken/BudgetService.java
+++ src/main/java/com/odde/securetoken/BudgetService.java
@@ -1,7 +1,6 @@
 package com.odde.securetoken;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import static java.time.temporal.ChronoUnit.DAYS;
@@ -19,26 +18,6 @@ public class BudgetService {
             return 0;
         }
         return queryBudget(new Period(startTime, endTime));
-    }
-
-    private int calBudgetMonth(LocalDate startTime, LocalDate endTime, List<Budget> all) {
-        int total = 0;
-        if (startTime.getYear() == endTime.getYear()) {
-            for (LocalDate current = startTime; current.isBefore(endTime) || current.isEqual(endTime); current = current.plusMonths(1)) {
-                if (current.getMonthValue() == startTime.getMonthValue()) {
-                    total += getBudgetOfPeriod(startTime, startTime.withDayOfMonth(startTime.lengthOfMonth()), all);
-                } else if (current.getMonthValue() == endTime.getMonthValue()) {
-                    total += getBudgetOfPeriod(endTime.withDayOfMonth(1), endTime, all);
-                } else {
-                    total += getBudgetOfPeriod(current.withDayOfMonth(1), current.withDayOfMonth(current.lengthOfMonth()), all);
-                }
-            }
-        }
-        return total;
-    }
-
-    private LocalDate endOfYear(int year) {
-        return LocalDate.of(year, Month.DECEMBER, 31);
     }
 
     private Budget findBudget(LocalDate startTime, List<Budget> all) {
@@ -64,24 +43,19 @@ public class BudgetService {
             return getBudgetOfPeriod(period.getStartTime(), period.getEndTime(), all);
         }
         //不是同一个月的 但是是同一年的
-        int total = calBudgetMonth(period.getStartTime(), period.getEndTime(), all);
-        if (total != 0) {
-            return total;
-        }
-        //不同年份的
-        for (int i = period.getStartTime().getYear(); i < period.getEndTime().getYear() + 1; i++) {
-            if (i == period.getStartTime().getYear()) {
-                total += calBudgetMonth(period.getStartTime(), endOfYear(period.getStartTime().getYear()), all);
-            } else if (i == period.getEndTime().getYear()) {
-                total += calBudgetMonth(startOfYear(period.getEndTime().getYear()), period.getEndTime(), all);
+        LocalDate startTime = period.getStartTime();
+        LocalDate endTime = period.getEndTime();
+        int total = 0;
+        for (LocalDate current = startTime; current.isBefore(endTime) || current.isEqual(endTime); current = current.plusMonths(1)) {
+            if (new Period(current, startTime).isSameMonth()) {
+                total += getBudgetOfPeriod(startTime, startTime.withDayOfMonth(startTime.lengthOfMonth()), all);
+            } else if (new Period(current, endTime).isSameMonth()) {
+                total += getBudgetOfPeriod(endTime.withDayOfMonth(1), endTime, all);
             } else {
-                total += calBudgetMonth(startOfYear(i), endOfYear(i), all);
+                total += getBudgetOfPeriod(current.withDayOfMonth(1), current.withDayOfMonth(current.lengthOfMonth()), all);
             }
         }
         return total;
     }
 
-    private LocalDate startOfYear(int year) {
-        return LocalDate.of(year, Month.JANUARY, 1);
-    }
 }
